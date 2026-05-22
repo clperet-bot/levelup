fc2289e4355d8180726ae5fd9ff6fa74c504ea84
LevelUp <clperet@gmail.com>
2026-05-22 22:09:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,19 @@
-// Service worker désactivé temporairement
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+{
+  "name": "LevelUp",
+  "short_name": "LevelUp",
+  "description": "Suivi des apprentissages",
+  "start_url": "/levelup/",
+  "scope": "/levelup/",
+  "display": "standalone",
+  "background_color": "#fff9f2",
+  "theme_color": "#ff6b35",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
