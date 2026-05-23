6c11e9ecaf0428c5f20995f4c5376cadd7906e9c
LevelUp <clperet@gmail.com>
2026-05-23 10:49:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,3 @@
-const CACHE_NAME = 'levelup-v2';
-const ASSETS = [
-  '/levelup/',
-  '/levelup/index.html',
-  '/levelup/manifest.json',
-  '/levelup/icon.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{}))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
